6e6eede7210d0ba1e6ee56252f3f55e0b798cb59
Dmitry Minin <32297125+xtrakTD@users.noreply.github.com>
2023-07-30 14:40:15 +0300

    add escape sequence

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+from pynput import keyboard
 from threading import Thread
 
 from src.AutoMailingBot import app, start_mailing
 from src.AdminBot import run_admin_bot
 
 
+def on_press(key, abortKey='esc'):    
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys    
+
+    print('pressed %s' % (k))
+    if k == abortKey:
+        print('end loop ...')
+        return False  # stop listener
+    
+
 def main():
+    abortKey = 'esc'
+    
+    listener = keyboard.Listener(on_press=on_press, abortKey=abortKey)
+    listener.start()
 
     Thread(target=start_mailing).start()
     Thread(target=run_admin_bot).start()
     app.run()
 
+    listener.join()
 
 if __name__ == '__main__':
     main()
