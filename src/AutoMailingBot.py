e847b78aa4580aaf471769531e87256a729b5298
Dmitry Minin <32297125+xtrakTD@users.noreply.github.com>
2023-07-30 13:04:38 +0300

    add try catch

--- src/AutoMailingBot.py
+++ src/AutoMailingBot.py
@@ -1,9 +1,15 @@
+import logging 
+
 from time import sleep
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from src.utils import *
+
+
+logger = logging.getLogger(__name__)
+
 
 CFG = get_cfg()
 
@@ -23,19 +29,31 @@ async def message_handler(client: Client, message: Message):
 
 def mail():
     settings = get_settings()
-    for chat_id in settings['ids']:
-        print(f'Sending message to {chat_id}')
-        app.send_message(chat_id, settings['text'])
-        sleep(INTERVAL)
+    _chat_ids = [x.chat.id for x in app.get_dialogs() if x.chat.id < 0]
+    
+    logger.info(f'list of chats: [{str(_chat_ids)}]')
+
+    # for chat_id in settings['ids']:
+    for chat_id in _chat_ids:
+        try:
+            print(f'Sending message to {chat_id}')
+            app.send_message(chat_id, settings['text'])
+            sleep(INTERVAL)
+        except Exception as e:
+            print(f'Unable to send message: [{str(chat_id)}], error: [{e}]')
+            logger.error(f'Unable to send message: [{str(chat_id)}], error: [{e}]')
 
 
 def start_mailing():
     while True:
         settings = get_settings()
         if check_time(settings['timer']):
-            mail()
-            new_timer = []
-            for i in settings['timer']:
-                new_timer.append(update_time(i, 7))
-            update_settings('timer', new_timer)
+            try:
+                mail()
+                new_timer = []
+                for i in settings['timer']:
+                    new_timer.append(update_time(i, 7))
+                update_settings('timer', new_timer)
+            except Exception as e: 
+                logger.error(f'Something went wrong: [{e}]')
         sleep(0.5)
